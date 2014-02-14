1c384ed87665969503423cda06e1d8fd60f56c42
samanthavandapuye <samantha.vandapuye@gmail.com>
2014-02-14 11:58:38 -0500

    value page update

--- wp-content/themes/springlawn/page-value-proposition.php
+++ wp-content/themes/springlawn/page-value-proposition.php
@@ -12,6 +12,30 @@
 			<?php the_content(); ?>
 		</div>
 		
+		<div class="cont box">
+			<p><strong>Founding Stewardship Investment Comparison</strong></p>
+			<table class="table">
+				<thead>
+					<th>year</th>
+					<th>lenox comps</th>
+					<th>berkshire legacy circle</th>
+				</thead>
+				<tbody>
+				<?php query_posts(array('post_type' => 'Investments',  'order' => 'asc')); ?>
+				<?php while ( have_posts() ) : the_post();  ?>
+					<tr class="member-cont">
+						<td><?php the_title(); ?></td>
+						<td><?php the_field('lenox_comps'); ?></td>
+						<td><?php the_field('berkshire'); ?></td>
+					</tr>
+				<?php endwhile; wp_reset_query(); ?>
+				</tbody>
+			</table>
+			<div class="bottom-cont">
+				<?php the_field('bottom_copy'); ?>
+			</div>
+		</div>
+		
 
 
 	<?php endwhile; // end of the loop. ?>
